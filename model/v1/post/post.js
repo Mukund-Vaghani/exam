ca0df4baf3663990b4ff1cd2a51cbb2a697aad12
ravit <ravit@hyperlinkinfosystem.net.in>
2023-04-20 17:05:11 +0530

    CONTACT US PENDING

--- model/v1/post/post.js
+++ model/v1/post/post.js
@@ -36,7 +36,7 @@ var post = {
             var condition = `WHERE p.is_active = 1 AND p.is_delete = 0 ORDER BY id DESC`;
         }
 
-        var sql = `SELECT p.* FROM tbl_post p join tbl_user u on p.user_id = u.id `;
+        var sql = `SELECT p.*,u.name FROM tbl_post p join tbl_user u on p.user_id = u.id `;
         sql += condition;
 
         con.query(sql, function (error, result) {
@@ -118,7 +118,92 @@ var post = {
                 callback("0","reset_keyword_something_wrong_message",null)
             }
         })
+    },
+
+
+
+// ******************************** EVENT ************************************************
+
+
+
+    addEvent: function(request,id,callback){
+        var eventDetail = {
+            user_id: id,
+            event_name: request.event_name,
+            event_address: request.event_address,
+            event_date: request.event_date,
+            event_time: request.event_time,
+            event_description: request.event_description,
+            event_image: request.event_image,
+            event_member: request.event_member,
+            event_nonmember: request.event_nonmember
+        }
+        con.query(`INSERT INTO tbl_event SET ?`,[eventDetail], function(error,result){
+            if(!error){
+                var event_id = result.insertId;
+                con.query(`SELECT *,CONCAT('${global.BASE_URL}','${global.EVENT_URL}',event_image) as event_image FROM tbl_event WHERE id = ?`,[event_id], function(error,result){
+                    if(!error){
+                        callback("1","reset_keyword_add_message",result)
+                    }else{
+                        callback("0","reset_keyword_edit_place_message",null);
+                    }
+                })
+            }else{
+                callback("0","reset_keyword_something_wrong_message",null);
+            }
+        })
+    },
+
+    searchEvent: function(request,callback){
+        con.query(`SELECT *,CONCAT('${global.BASE_URL}','${global.EVENT_URL}',event_image) as event_image FROM tbl_event WHERE event_date IN ('${request.date}') ORDER BY event_time`,function(error,result){
+            if(!error){
+                callback("1","reset_keyword_success_message",result)
+            }else{
+                callback("0","reset_keyword_something_wrong_message",null);
+            }
+        })
+    },
+
+
+
+// ********************************* FAQ *************************************************
+
+
+
+    faq: function(request,callback){
+        if (request.search != undefined) {
+            var condition = `where question like '%${request.search}%'`;
+        } else {
+            var condition = `WHERE is_active = 1 AND is_delete = 0`;
+        }
+
+        var sql = `SELECT * FROM tbl_faq `;
+        sql += condition;
+
+        con.query(sql, function (error, result) {
+            if (!error && result.length > 0) {
+                callback("1", "reset_keyword_success_message", result);
+            } else {
+                console.log(error);
+                callback("0", "reset_keyword_something_wrong_message", null);
+            }
+        })
+    },
+
+
+// *********************************** CONTACT US *****************************************
+
+
+    contactUs: function(request,callback){
+        var contactDetail = {
+            title: request.title,
+            email: request.email,
+            message: request.message
+        }
+
+        con.query(`INSERT INTO tbl_contact_us SET ?`)
     }
+
 }
 
 module.exports = post
